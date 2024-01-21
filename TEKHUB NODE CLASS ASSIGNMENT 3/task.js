5f2edeb33268141a7e367627282b86df0ace5bc1
EMMATECH25 <156687229+EMMATECH25@users.noreply.github.com>
2024-01-21 10:23:58 -0800

    Add files via upload

---
+++ TEKHUB NODE CLASS ASSIGNMENT 3/task.js
@@ -0,0 +1,50 @@
+
+
+let person = {
+    name: 'Emmanuel',
+    age:26,
+    
+};
+
+person['likes food'] = true;
+
+let habit = 'likes food';
+person[habit];
+console.log(person[habit]);
+
+
+let device = 'andriod';
+
+let phone ={
+    [device]:'Tecno'
+};
+
+console.log(phone.andriod);
+
+let league = {
+    winner: 'liverpool',
+    isRelegated:true,
+    teamMate:'bowen',
+    fourth:'westham',
+};
+
+delete league.isRelegated
+
+delete league.fourth
+
+delete league.teamMate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
